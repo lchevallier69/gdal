45547b56689cedc87274848e3a4f9db97708f08b
CHEVALLIER <lchevallier@cirilgroup.com>
2022-01-03 15:25:42 +0100

    fix debug issue in vs

---
+++ apps/env.bat
@@ -0,0 +1,9 @@
+set CPL_DEBUG=ON
+
+set DWG_ATTRIBUTES=TRUE
+
+set DXF_INLINE_BLOCKS=FALSE                                        
+
+set GDAL_DATA=D:\users\lcvr\WP2\gdal\gdal\data
+
+set PATH=%PATH%;D:\users\lcvr\WP2\gdal\oda\22.9\Drawings_vc16_amd64dll_22.9\exe\vc16_amd64dll;D:\users\lcvr\WP2\gdal\oda\22.9\Kernel_vc16_amd64dll_22.9\exe\vc16_amd64dll
